69cb1830cd19eea2687e995e3a59efd9edd1165e
Ronen Boneh <rboneh@live.com>
2026-01-01 10:03:31 +0200

    Update project: reorganized blogs, added members/exhibitions/pictures

--- index.js
+++ index.js
@@ -1,49 +1,117 @@
-import { constants } from "buffer";
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./.env" });
+
+import "dotenv/config";
+
 import express from "express";
 import fs from "fs";
-import bodyParser from "body-parser";
-
+// use built-in express body parser
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import * as u from "./public/utilities.js";
 
+import { v2 as cloudinary } from 'cloudinary';
+
+const cloudinaryCloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const cloudinaryApiKey = process.env.CLOUDINARY_API_KEY;
+const cloudinaryApiSecret = process.env.CLOUDINARY_API_SECRET;
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const dirName = dirname(fileURLToPath(import.meta.url));
-// console.log(__dirname + "/public/index.html");
+
+const membersDir = __dirname + "/public/members";
+const exhibitionDir = __dirname + "/public/exhibition";
+console.log(membersDir);
+console.log(exhibitionDir);
+var memberList = [];
+var exhibitionList = [];
+
+fs.readdir(membersDir, (err, files) => {
+  if (err) {
+    console.error('Error reading directory:', err);
+    return;
+  }
+
+  // Get all members
+  console.log('Files in directory:');
+  files.forEach(file => {
+    if (file != '.DS_Store') {
+      memberList.push(file);
+    }
+  });
+  console.log(memberList);
+});
+
+//Cloudinary configuration
+(async function () {
+
+  // Configuration
+  cloudinary.config({
+    cloud_name: cloudinaryCloudName,
+    api_key: cloudinaryApiKey,
+    api_secret: cloudinaryApiSecret // Click 'View API Keys' above to copy your API secret
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(
+      'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+      public_id: 'shoes',
+    }
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+
+  console.log(uploadResult);
+
+  // Optimize delivery by resizing and applying auto-format and auto-quality
+  const optimizeUrl = cloudinary.url('shoes', {
+    fetch_format: 'auto',
+    quality: 'auto'
+  });
+
+  console.log(optimizeUrl);
+
+  // Transform the image: auto-crop to square aspect_ratio
+  const autoCropUrl = cloudinary.url('shoes', {
+    crop: 'auto',
+    gravity: 'auto',
+    width: 500,
+    height: 500,
+  });
+
+  console.log(autoCropUrl);
+})();
+
+
+
+
+//List of all members photos for carousel
+var membersPhotos = u.getFiles(membersDir);
+//console.log(membersPhotos);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var messageBody = {};
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-function getBodyFields(req, res, next) {
-  // const title = req.body.blogTitle;
-  // const email = req.body.authorEmail;
-  // const name = req.body.authorName;
-  // const text = req.body.blogText;
-  const messageBody = req.body;
-  // console.log("Email is: " + email);
-  // console.log(text);
-  next();
-}
+// configure EJS views
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.use(getBodyFields);
+
+//Express routing
 
 app.get("/", (req, res) => {
   res.render("partials/index.ejs");
-  // res.sendFile(__dirname + "/public/index.html");
-  // res.sendStatus(201);
-  // console.log("Manage get / request");
 });
 
 app.get("/home", (req, res) => {
   res.render("index.ejs");
-  // res.sendFile(__dirname + "/public/index.html");
-  // res.sendStatus(201);
-  // console.log("Manage get / request");
 });
 
 app.get("/about", (req, res) => {
@@ -56,7 +124,15 @@ app.get("/contact", (req, res) => {
   res.sendStatus(201);
 });
 
-app.get("/photography", (req, res) => {
+app.get("/members", (req, res) => {
+  // console.log('manage /photo');
+  res.render("partials/index.ejs", {
+    member: "any",
+    themeImage: "https://picsum.photos/id/91/3504/2336?random=1",
+  });
+});
+
+app.get("/exhibitions", (req, res) => {
   // console.log('manage /photo');
   const fileList = u.getFiles(__dirname + "/views/blogs/photography");
   const tableBody = u.makeTableBody(fileList, "Photography");
@@ -64,66 +140,6 @@ app.get("/photography", (req, res) => {
     tableBody: tableBody,
     themeImage: "https://picsum.photos/id/91/800/200?random=1",
   });
-});
-
-app.get("/blog-form", (req, res) => {
-  const filePath = __dirname + "/views/partials/blog-form.ejs";
-  const blogForm = u.readFileSynchronously(filePath);
-  res.render("partials/index.ejs", {
-    blogForm: blogForm,
-    themeImage: "https://picsum.photos/id/180/800/100?random=1",
-  });
-});
-
-app.post("/get-blog", (req, res) => {
-  const reqBody = req.body;
-  // console.log("get-blog:");
-  // console.log(reqBody);
-
-  const fDate = u.date4fileName(reqBody.fileDate);
-  const fileName = `/views/blogs/${reqBody.blogsId}/${reqBody.fileName}-${fDate}.txt`;
-  const filePath = __dirname + fileName;
-  const blog = u.readFileSynchronously(filePath);
-  const fBlog = u.formatBlog(blog);
-
-  // console.log(fBlog);
-  res.render("partials/index.ejs", {
-    blogForm: fBlog,
-    themeImage: "https://picsum.photos/id/180/800/100?random=1",
-  });
-  // res.sendStatus(201);
-});
-
-app.post("/new-blog", (req, res) => {
-  // Your code here
-  const formData = req.body;
-  // console.log(formData);
-
-  // writing new blog to db
-  const blogObj = u.createBlogFile(formData, dirName);
-
-  fs.writeFile(blogObj.filePath, blogObj.nBlog, function (err) {
-    if (err) throw err;
-    console.log("Saved!");
-
-    // Read the files or render the updated page after the writing process is complete
-    const fileList = u.getFiles(__dirname + "/views/blogs/photography");
-    //console.log(fileList);
-    const tableBody = u.makeTableBody(fileList, formData.postId);
-    res.render("partials/index.ejs", {
-      tableBody: tableBody,
-      themeImage: "https://picsum.photos/id/91/800/200?random=1",
-    });
-  });
-
-  //sending client update blogs table
-  // const fileList = u.getFiles(__dirname + "/views/blogs/photography");
-  // console.log(fileList);
-  // const tableBody = u.makeTableBody(fileList, formData.postId);
-  // res.render("partials/index.ejs", {
-  //   tableBody: tableBody,
-  //   themeImage: "https://picsum.photos/id/91/800/200?random=1",
-  // });
 });
 
 app.listen(port, () => {
